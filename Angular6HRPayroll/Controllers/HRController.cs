ee183a8bef5d67d6b7089d0fcfff69eb0404ff80
amar10063 <amar10063@yahoo.co.in>
2018-12-07 18:03:44 +0530

    added company page

---
+++ Angular6HRPayroll/Controllers/HRController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular6HRPayroll.Controllers
+{
+    public class HRController : Controller
+    {
+        // GET: HR
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Company()
+        {
+            return View();
+        }
+    }
+}

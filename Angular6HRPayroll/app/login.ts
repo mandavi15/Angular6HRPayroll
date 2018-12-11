a24973f345eff5d980e3492b0c1233ff1bad8b9b
mandavi15 <er.mandavi@gmail.com>
2018-12-11 17:21:46 +0530

    added designation page

---
+++ Angular6HRPayroll/app/login.ts
@@ -0,0 +1,37 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'login',
+    templateUrl: './Login/Login.cshtml',
+})
+
+export class AppComponent implements OnInit {
+    loginForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.loginForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)')
+    }
+}
---
+++ Angular6HRPayroll/dist/login.js
@@ -0,0 +1,51 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
+var AppComponent = /** @class */ (function () {
+    function AppComponent(formBuilder) {
+        this.formBuilder = formBuilder;
+        this.submitted = false;
+    }
+    AppComponent.prototype.ngOnInit = function () {
+        this.loginForm = this.formBuilder.group({
+            firstName: ['', forms_1.Validators.required],
+            lastName: ['', forms_1.Validators.required],
+            email: ['', [forms_1.Validators.required, forms_1.Validators.email]],
+            password: ['', [forms_1.Validators.required, forms_1.Validators.minLength(6)]]
+        });
+    };
+    Object.defineProperty(AppComponent.prototype, "f", {
+        // convenience getter for easy access to form fields
+        get: function () { return this.loginForm.controls; },
+        enumerable: true,
+        configurable: true
+    });
+    AppComponent.prototype.onSubmit = function () {
+        this.submitted = true;
+        // stop here if form is invalid
+        if (this.loginForm.invalid) {
+            return;
+        }
+        alert('SUCCESS!! :-)');
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'login',
+            templateUrl: '~/Login/Login.cshtml',
+        }),
+        __metadata("design:paramtypes", [forms_1.FormBuilder])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=login.js.map

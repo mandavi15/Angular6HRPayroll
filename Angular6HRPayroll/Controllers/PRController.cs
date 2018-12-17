8fa9bafc126de1e3173a0efb866865fbe2c899d4
abhishek <abhishekvats367@gmail.com>
2018-12-17 14:27:18 +0530

    payroll

---
+++ Angular6HRPayroll/Controllers/PRController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular6HRPayroll.Controllers
+{
+    public class PRController : Controller
+    {
+        // GET: PR
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult setup()
+        {
+            return View();
+        }
+    }
+}
